b3fb9c11221d0e96c29120508cf2da7c7a87553f
Terry Babyuk <tbabyuk@gmail.com>
2024-06-21 23:04:50 -0400

    second commit, started working with TMDB api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
 function App() {
+
+  const handleApiCall = () => {
+    console.log("Hello World")
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ${process.env.TMDB_ACCESS_TOKEN}'
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=comedy', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }
+
+
   return (
     <div className="App">
       Movie Finder
+      <button onClick={handleApiCall}>Make API Call</button>
     </div>
   );
 }
