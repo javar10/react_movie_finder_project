1e62fcee4793292ea7a4fd61a714df78d917994b
Terry Babyuk <tbabyuk@gmail.com>
2024-06-26 11:05:27 -0400

    figured out how to use TMDB with different auth methods

--- src/App.js
+++ src/App.js
@@ -27,14 +27,56 @@ function App() {
   }
 
   // console.log(data);
-  data === null ? console.log('null data') : console.log(data);
+  // data === null ? console.log('null data') : console.log(data);
+
+
+
+  const handleTerryApiCallWithApiKey = async () => {
+
+    // const res = await fetch("https://api.themoviedb.org/3/movie/11?api_key=6cd761657add0190cde357202b274567")
+
+    // step 1: query movie by name to get movie ID
+    // const res = await fetch("https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=6cd761657add0190cde357202b274567")
+
+
+    // step 2: use movie id to query movie details
+    const res = await fetch("https://api.themoviedb.org/3/movie/75780?api_key=6cd761657add0190cde357202b274567")
+
+
+    const data = await res.json();
+
+    console.log("logging fetch result from ApiKey Fetch Call:", data)
+  }
+
+
+  const handleTerryApiCallWithBearerToken = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        // Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2Q3NjE2NTdhZGQwMTkwY2RlMzU3MjAyYjI3NDU2NyIsIm5iZiI6MTcxOTQxMTA0OC42OTgyMDQsInN1YiI6IjY2NzYzMThiY2U2ODE3MzU5YmVkMWI0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9vHbWzMtRcDxQ44RuX6EdjQv7xXYJ4cDSUGmaACfn84'
+      }
+    };
+    // const res = await fetch("https://api.themoviedb.org/3/movie/11", options)
+
+    // step 1: query movie by name to get movie ID
+    // const res = await fetch("https://api.themoviedb.org/3/search/movie?query=Jack+Reacher", options)
+
+    // step 2: use movie id to query movie details
+    // const res = await fetch("https://api.themoviedb.org/3/movie/75780", options)
+
+    // const data = await res.json();
+
+    console.log("logging fetch result from Bearer Token Fetch Call:", data)
+  }
   
 
   return (
     <div className="App">
       Movie Finder
       <div>
-        <button onClick={handleApiCall}>Make API Call</button>
+        <button onClick={handleTerryApiCallWithApiKey}>Make API Call</button>
       </div>
       <div>
         {data === null ? <></> : 
